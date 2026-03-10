7bdc5c387cafdc603bbebb73440f4486818d0011
Andrew <anderybro@gmail.com>
2026-03-10 21:13:12 +0300

    Completed

--- lessons/Stepik_6.2.py
+++ lessons/Stepik_6.2.py
@@ -1,5 +1,6 @@
 city1, city2, city3 = input(), input(), input()
 len1, len2, len3 = len(city1), len(city2), len(city3)
+
 if len1 < len2 and len1 < len3:
     shortest = city1
 elif len2 < len1 and len2 < len3:
@@ -13,5 +14,6 @@ elif len2 > len1 and len2 > len3:
     longest = city2
 else:
     longest = city3
+
 print(shortest)
 print(longest)
